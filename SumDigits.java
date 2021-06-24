87ff8f9a570852ee03edafee82c35a8f37b55f75
Cordell <cordellbonnieux@gmail.com>
2021-06-24 14:38:45 -0700

    program complete =)

--- SumDigits.java
+++ SumDigits.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 public class SumDigits {
     public static void main(String[] args) {
-        
 
         // create scanner object
         Scanner input = new Scanner(System.in);
 
         // prompt and read "a" then "b"
-        int a = readNum(input);
-        int b = readNum(input);
+        int a = readNum(input),
+            b = readNum(input);
 
-        // check if the value's even digits are equal
-        Boolean isEven = gatherSum(a, b);
+        // get the sum of the even digits in each integer
+        int evenA = sumEven(a), 
+            evenB = sumEven(b);
+        
+        // print the sum of the even numbers
+        System.out.printf("The sum of %d's even digits is %d \n", a, evenA);
+        System.out.printf("The sum of %d's even digits is %d \n", b, evenB);
 
-
+        // print if sums of evens are equal
+        System.out.println("Are the sums of each number's even integers the same? " + isEqual(evenA, evenB));
     }
 
     // reads an integer; if negative, method is called again; if positive, integer is retruned
@@ -31,20 +36,6 @@ public class SumDigits {
         else  
             // call the function again if the integer is invalid  
             return readNum(input); 
-
-    }
-
-    public static Boolean gatherSum(int a, int b) {
-
-        int evenA = sumEven(a), evenB = sumEven(b);
-
-        // test
-        int lengthA = getLength(a),
-            lengthB = getLength(b);
-        System.out.printf("%d has %d digits and the sum of the even integers is %d \n", a, lengthA, evenA);
-        System.out.printf("%d has %d digits and the sum of the even integers is %d \n", b, lengthB, evenB);
-        return true;
-
     }
 
     // returns the sum of even digits in a number
@@ -59,28 +50,31 @@ public class SumDigits {
             int digit = z % 10;
 
             // if digit is divisible by 2 add it to even.
-            if (digit % 2 == 0) even += digit;
+            if (digit % 2 == 0) 
+                even += digit;
 
             // divide z by ten to get next digit
             z /= 10;
         }
 
+        // check the last digit
+        if (z < 10 && z % 2 == 0) 
+            even += z;
+
         // return the sum of even digits
         return even;
     }
 
-    // returns the number of digits in an integer
-    public static int getLength(int y) {
+    // returns true if the numbers are equal, false otherwise
+    public static Boolean isEqual(int a, int b) {
+        
+        // is equal
+        if (a == b) 
+            return true;
 
-        // used to keep track of length
-        int length = 0;
-
-        // get the number of digits in y
-        while (y > 0) {
-            y /= 10;
-            length++;
-        }
-
-        return length;
+        // is not equal
+        else 
+            return false;
     }
+
 }
