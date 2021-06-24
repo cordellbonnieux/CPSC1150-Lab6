b6b6d8f41e9e6193d134637c6b2805de7660a3e5
Cordell <cordellbonnieux@gmail.com>
2021-06-24 13:53:55 -0700

    Program now works as intended, just needs to be organized.

--- SumDigits.java
+++ SumDigits.java
@@ -16,6 +16,7 @@ public class SumDigits {
 
     }
 
+    // reads an integer; if negative, method is called again; if positive, integer is retruned
     public static int readNum(Scanner input) {
 
         // prompt user
@@ -35,29 +36,51 @@ public class SumDigits {
 
     public static Boolean gatherSum(int a, int b) {
 
-        int lengthA = 0, lengthB = 0, y = a, z = b;
-
-        // get the length of a
-        while (y > 0) {
-            y /= 10;
-            lengthA++;
-        }
-        // get the length of b
-        while (z > 0) {
-            z /= 10;
-            lengthB++;
-        }
-
-        
-
-
-
-
+        int evenA = sumEven(a), evenB = sumEven(b);
 
         // test
-        System.out.printf("%d has %d digits \n", a, lengthA);
-        System.out.printf("%d has %d digits \n", b, lengthB);
+        int lengthA = getLength(a),
+            lengthB = getLength(b);
+        System.out.printf("%d has %d digits and the sum of the even integers is %d \n", a, lengthA, evenA);
+        System.out.printf("%d has %d digits and the sum of the even integers is %d \n", b, lengthB, evenB);
         return true;
 
+    }
+
+    // returns the sum of even digits in a number
+    public static int sumEven(int z) {
+
+        // store the sum of evens
+        int even = 0;
+
+        while (z >= 10) {
+
+            // isolate digit
+            int digit = z % 10;
+
+            // if digit is divisible by 2 add it to even.
+            if (digit % 2 == 0) even += digit;
+
+            // divide z by ten to get next digit
+            z /= 10;
+        }
+
+        // return the sum of even digits
+        return even;
+    }
+
+    // returns the number of digits in an integer
+    public static int getLength(int y) {
+
+        // used to keep track of length
+        int length = 0;
+
+        // get the number of digits in y
+        while (y > 0) {
+            y /= 10;
+            length++;
+        }
+
+        return length;
     }
 }
