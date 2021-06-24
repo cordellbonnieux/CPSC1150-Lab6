5ca3478736015e052a73c7353e09810c808203cb
Cordell <cordellbonnieux@gmail.com>
2021-06-24 09:45:56 -0700

    Started second program

---
+++ SumDigits.java
@@ -0,0 +1,39 @@
+public class SumDigits {
+    import java.util.Scanner;
+    public static void main(String[] args) {
+
+        // create scanner object
+        Scanner input = new Scanner(System.in);
+
+        // prompt and read "a" then "b"
+        int a = readNum(input);
+        int b = readNum(input);
+
+        // check if the value's even digits are equal
+        Boolean isEven = sumOfDigits(a, b);
+
+
+    }
+    public static int readNum(Scanner input) {
+
+        //prompt
+        System.out.print("Please enter an integer: ");
+        // read and return value
+        return int x = input.nextInt();
+
+    }
+
+    public static Boolean sumOfDigits(int a, int b) {
+
+        int sumA, sumB, length;
+
+      int i = 0;
+
+      for (i; i < 10; i++) {
+
+        
+
+      }
+
+    }
+}
